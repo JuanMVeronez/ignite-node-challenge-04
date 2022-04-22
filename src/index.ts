a1e40078f32bb1f063145f3446eb0d25c100c6b8
Juan Veronez <juan.monteiro@venturus.org.br>
2022-04-21 22:01:29 -0300

    add swagger to the project

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
+import { setup, serve } from "swagger-ui-express";
 
+import swaggerSetup from "../swagger.json";
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
@@ -7,5 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/users", usersRoutes);
+
+app.use("/api-docs", serve, setup(swaggerSetup));
 
 export { app };
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,8 +9,9 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    const userIsAdmin = this.usersRepository.findById(user_id).admin;
-    if (!userIsAdmin) throw new Error("Only admins allowed");
+    const userIsAdmin = this.usersRepository.findById(user_id)?.admin;
+    if (!userIsAdmin)
+      throw new Error("You need to be a administrator to list the users");
 
     return this.usersRepository.list();
   }
